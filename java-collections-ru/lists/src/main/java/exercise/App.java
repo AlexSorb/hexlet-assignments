5367225287dcb0f48fe787c4414eb0fbd3a56610
@hexlet/cli <support@hexlet.io>
2024-02-13 12:59:13 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -2,28 +2,22 @@ package exercise;
 
 //import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 
 // BEGIN
 public  class App {
     public static boolean scrabble(String characters, String word) {
-        if (characters.length() < word.length()) {
-            return false;
-        }
 
-        List<Character> listCharactersOfWord = new ArrayList<>();
-        for (var sw: word.toLowerCase().toCharArray()) {
-            listCharactersOfWord.add(sw);
-        }
-        List<Character> charArrays = new ArrayList<>();
-        for (var sw: characters.toLowerCase().toCharArray()) {
-            charArrays.add(sw);
-        }
-        for (var ch: listCharactersOfWord) {
-            if (!charArrays.contains(ch)) {
+        List<String> listSymbolsOfWord = new ArrayList<>(Arrays.asList(word.toLowerCase().split("")));
+        List<String> listSymbolsOfCharacters =
+                new ArrayList<>(Arrays.asList(characters.toLowerCase().split("")));
+        for (String character: listSymbolsOfWord) {
+            if (!listSymbolsOfCharacters.contains(character)) {
                 return false;
             }
-            charArrays.remove(charArrays.indexOf(ch));
+            listSymbolsOfCharacters.remove(listSymbolsOfCharacters.indexOf(character));
         }
         return true;
     }
