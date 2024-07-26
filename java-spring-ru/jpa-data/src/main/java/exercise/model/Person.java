0e592dbb97aac9aa4369f5f1e0d97bfaf289b392
@hexlet/cli <support@hexlet.io>
2024-07-26 17:17:46 +0300

    submit java-spring-ru/dto-mapper

--- java-spring-ru/jpa-data/src/main/java/exercise/model/Person.java
+++ java-spring-ru/jpa-data/src/main/java/exercise/model/Person.java
@@ -1,23 +1,27 @@
 package exercise.model;
 
-import jakarta.persistence.Id;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import static jakarta.persistence.GenerationType.IDENTITY;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 // BEGIN
 @Entity
-@Table(name = "persons")
+@Table(name = "people")
 @Getter
 @Setter
 public class Person {
     @Id
     @GeneratedValue(strategy = IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
+
+    @Column(unique = true)
     private String firstName;
+    @Column(unique = true)
     private String lastName;
 }
 // END
