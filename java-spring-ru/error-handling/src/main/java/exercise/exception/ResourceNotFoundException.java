f3c4234beecdd660cad72abab6d4282a73d1c093
@hexlet/cli <support@hexlet.io>
2024-08-07 14:30:55 +0300

    submit java-spring-ru/authentication

--- java-spring-ru/error-handling/src/main/java/exercise/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package exercise.exception;
-
-// BEGIN
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String massage) {
-        super(massage);
-    }
-}
-// END
